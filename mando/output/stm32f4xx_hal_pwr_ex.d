d7c9dc0705590ffbcc7abef6512a9641e6e366aa
JavierMartinContreras <129075741+JavierMartinContreras@users.noreply.github.com>
2023-04-02 10:51:54 +0200

    Add files via upload
    Versión de la entrega intermedia del Skate.

---
+++ mando/output/stm32f4xx_hal_pwr_ex.d
@@ -0,0 +1,56 @@
+output/stm32f4xx_hal_pwr_ex.o: \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ port/nucleo_stm32f446re/include/stm32f4xx_hal_conf.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ ../../drivers/stm32f4xx/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ ../../drivers/stm32f4xx/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ ../../drivers/stm32f4xx/CMSIS/Include/core_cm4.h \
+ ../../drivers/stm32f4xx/CMSIS/Include/cmsis_version.h \
+ ../../drivers/stm32f4xx/CMSIS/Include/cmsis_compiler.h \
+ ../../drivers/stm32f4xx/CMSIS/Include/cmsis_gcc.h \
+ ../../drivers/stm32f4xx/CMSIS/Include/mpu_armv7.h \
+ ../../drivers/stm32f4xx/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ ../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+port/nucleo_stm32f446re/include/stm32f4xx_hal_conf.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+../../drivers/stm32f4xx/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+../../drivers/stm32f4xx/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+../../drivers/stm32f4xx/CMSIS/Include/core_cm4.h:
+../../drivers/stm32f4xx/CMSIS/Include/cmsis_version.h:
+../../drivers/stm32f4xx/CMSIS/Include/cmsis_compiler.h:
+../../drivers/stm32f4xx/CMSIS/Include/cmsis_gcc.h:
+../../drivers/stm32f4xx/CMSIS/Include/mpu_armv7.h:
+../../drivers/stm32f4xx/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+../../drivers/stm32f4xx/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
